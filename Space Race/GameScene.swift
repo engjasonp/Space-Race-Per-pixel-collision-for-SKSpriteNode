f2e062a4d6531d3a088279e0fe9edb2ea18986a5
engjasonp <engjasonp@gmail.com>
2015-09-13 15:01:00 -0700

    Added starfield, player, scoreLabel, and score properties to class. Updated func didMoveToView() to add "Starfield.sks", "player.png", and scoreLabel to view.

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -8,9 +8,42 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
     override func didMoveToView(view: SKView) {
-
+        backgroundColor = UIColor.blackColor()
+        
+        let starfieldPath = NSBundle.mainBundle().pathForResource("Starfield", ofType: "sks")!
+        starfield = NSKeyedUnarchiver.unarchiveObjectWithFile(starfieldPath) as! SKEmitterNode
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture, size: player.size)
+        player.physicsBody!.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .Left
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
