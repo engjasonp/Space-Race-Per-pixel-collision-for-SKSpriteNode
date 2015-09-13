1d6680da78257721a6f6d8825713c0d0d4ea2cc7
engjasonp <engjasonp@gmail.com>
2015-09-13 14:39:57 -0700

    Initial Commit

---
+++ Space RaceTests/Space_RaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Space_RaceTests.swift
+//  Space RaceTests
+//
+//  Created by Jason Eng on 9/13/15.
+//  Copyright (c) 2015 EngJason. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Space_RaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
