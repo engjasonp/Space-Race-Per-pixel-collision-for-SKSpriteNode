ff334609871a5957533cfd524d3c6a3e031c653d
engjasonp <engjasonp@gmail.com>
2015-09-13 15:11:22 -0700

    Added possibleEnemies, gameTimer, and gameOver properties to class. Added gameTimer settings to didMoveToView. Made func createEnemy() to spawn a random sprite from possibleEnemies and specified settings for its physics.

--- Space Race/GameScene.swift
+++ Space Race/GameScene.swift
@@ -19,6 +19,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: NSTimer!
+    var gameOver = false
+    
     override func didMoveToView(view: SKView) {
         backgroundColor = UIColor.blackColor()
         
@@ -44,6 +48,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(0.35, target: self, selector: "createEnemy", userInfo: nil, repeats: true)
+    }
+    
+    func createEnemy() {
+        possibleEnemies.shuffle()
+        
+        let sprite = SKSpriteNode(imageNamed: possibleEnemies[0])
+        sprite.position = CGPoint(x: 1200, y: RandomInt(min: 50, max: 736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
